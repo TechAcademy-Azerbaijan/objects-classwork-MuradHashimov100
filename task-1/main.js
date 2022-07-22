a4282315e8c167e05c8bf7f4beb2a38eac6ba72a
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-07-22 20:36:08 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,7 +1,7 @@
 let products = [
     {title:'Apple', weight: 12}, 
     {title:'Chocolate', weight: 43}, 
-    {title:'Avocado', weight: 33}, 
+    {title:'Avocados', weight: 33}, 
     {title:'Milk', weight: 32}, 
     {title:'Bread', weight: 18}, 
     {title:'Cherry', weight: 65}, 
@@ -10,6 +10,14 @@ let products = [
     {title:'Kiwi', weight: 7}
 ]
 
-let foods = ['Apple', 'Avocados', 'Cherry', 'Orange', 'Kiwi']
+let foods = ['Apple', 'Avocados', 'Cherry', 'Orange', 'Kiwi'];
+// let forum  = for(let i = 0;i<foods.length;i++){
 
-// write code here
+// }
+let sum = 0;
+for(let obj of products){
+   if(foods.includes(obj.title)){
+    sum = sum + obj.weight;
+   }
+}
+console.log(sum);
